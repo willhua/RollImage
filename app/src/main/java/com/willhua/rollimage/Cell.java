5a5e55557fd2b5b55c14179e85b079d7c709984d
willhua <znjxboy@163.com>
2016-09-03 23:57:05 +0800

    finish cell

--- app/src/main/java/com/willhua/rollimage/Cell.java
+++ app/src/main/java/com/willhua/rollimage/Cell.java
@@ -1,11 +1,48 @@
 package com.willhua.rollimage;
 
-import android.graphics.Rect;
+import android.graphics.RectF;
 
 /**
  * Created by willhua on 2016/9/3.
  */
 public class Cell {
-    public Rect mRect;
-    public int mAlpha;
+    private RectF mRectF;
+    private int mAlpha;
+
+    public Cell(){
+        mRectF = new RectF();
+        mAlpha = 0;
+    }
+
+    public Cell(RectF rectF, int alpha){
+        mRectF = rectF;
+        mAlpha = alpha;
+    }
+
+    public void setWidth(float width){
+        float diff = (width - mRectF.width()) / 2;
+        mRectF.left -= diff;
+        mRectF.right += diff;
+    }
+
+    public void moveVertical(float value){
+        mRectF.top += value;
+        mRectF.bottom += value;
+    }
+
+    public void setRectF(RectF rectF){
+        mRectF = rectF;
+    }
+
+    public void setAlpha(int alpha){
+        mAlpha = alpha;
+    }
+
+    public RectF getmRectF(){
+        return mRectF;
+    }
+
+    public int getAlpha(){
+        return  mAlpha;
+    }
 }
