3ec144562e451d645bfedacde535e5fd5e7dc97a
willhua <znjxboy@163.com>
2016-09-03 15:42:39 +0800

    Initial commit

---
+++ app/src/main/java/com/willhua/rollimage/ImageLoader.java
@@ -0,0 +1,8 @@
+package com.willhua.rollimage;
+
+/**
+ * Created by willhua on 2016/9/3.
+ */
+public interface ImageLoader {
+    public 
+}
---
+++ app/src/main/java/com/willhua/rollimage/RollImageView.java
@@ -0,0 +1,16 @@
+package com.willhua.rollimage;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by willhua on 2016/9/3.
+ */
+public class RollImageView extends View {
+
+
+    public RollImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
